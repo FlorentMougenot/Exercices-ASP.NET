377c409e518db90a52e2dc52a6c5d22e307ed159
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2024-02-01 15:05:48 +0100

    feat : add platypus

---
+++ ExerciceOrnithorynque/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ExerciceOrnithorynque.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExerciceOrnithorynque.Data
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Platypus> PlatypusOriginal { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog = Playtypus;User ID=SA;Password=192Panda;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+        }
+    }
+}
---
+++ ExerciceOrnithorynque/Migrations/20240131153700_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExerciceOrnithorynque.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlatypusOriginal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Size = table.Column<double>(type: "float", nullable: true),
+                    Weight = table.Column<double>(type: "float", nullable: true),
+                    CanSwim = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlatypusOriginal", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlatypusOriginal");
+        }
+    }
+}
